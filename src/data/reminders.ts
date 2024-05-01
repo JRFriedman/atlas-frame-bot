23e5e67d0ffe548c00e56c519e91e719b25de865
Jacob <jacob@joinatlas.xyz>
2024-05-01 17:42:47 -0400

    fix chron job to go every 30 minutes

--- src/data/reminders.ts
+++ src/data/reminders.ts
@@ -1,4 +1,4 @@
-export default [['0 * * * *', 'This is a test cast, but feel free to check out www.atlascasts.com until we are up and running']] as [
+export default [['*/30 * * * *', 'This is a test cast, but feel free to check out www.atlascasts.com until we are up and running']] as [
   string,
   string
 ][]
