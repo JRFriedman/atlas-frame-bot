92e4050c795769a2df867b9201057305f78c118b
Jacob <jacob@joinatlas.xyz>
2024-05-01 22:15:12 -0400

    revise cast message

--- src/data/reminders.ts
+++ src/data/reminders.ts
@@ -1,1 +1,1 @@
-export default [['*/10 * * * *', 'This is a test cast, but feel free to check out www.atlascasts.com until we are up and running']] as [string, string][];
+export default [['*/10 * * * *', 'This is a scheduled cast, but feel free to check out www.atlascasts.com until we are up and running']] as [string, string][];
