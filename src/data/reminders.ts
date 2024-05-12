8d0a32e7da806cdf42fd409dd85628faf041d072
Jacob <jacob@joinatlas.xyz>
2024-05-11 22:31:41 -0400

    update messages

--- src/data/reminders.ts
+++ src/data/reminders.ts
@@ -4,7 +4,7 @@ export default [
     ['0 12 * * *', "Every conversation is a chance to gain a new understanding."],
     ['0 15 * * *', "Conversations can turn acquaintances into friends and ideas into movements."],
     ['0 18 * * *', "Conversations are where ideas flow and bonds form."],
-    ['0 21 * * *', "True conversation often lies in the silence between the words."],
+    ['0 21 * * *', "True conversation often lies in the silence between words."],
     ['59 23 * * *', "A conversation can add insight to any moment."]
 
 ] as [string, string][];
