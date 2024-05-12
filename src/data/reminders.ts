030457dbd09dc37ad45112b35781b561356d2f4f
Jacob <jacob@joinatlas.xyz>
2024-05-11 22:31:59 -0400

    update message

--- src/data/reminders.ts
+++ src/data/reminders.ts
@@ -1,6 +1,6 @@
 export default [
 
-    ['0 9 * * *', "Good conversations strengthen our connections and broaden our perspectives."],
+    ['0 9 * * *', "Conversations strengthen our connections and broaden our perspectives."],
     ['0 12 * * *', "Every conversation is a chance to gain a new understanding."],
     ['0 15 * * *', "Conversations can turn acquaintances into friends and ideas into movements."],
     ['0 18 * * *', "Conversations are where ideas flow and bonds form."],
