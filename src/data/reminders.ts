5e4f1e17dd63d8e24af6f7c5d3eb085777a5d7a6
Jacob <jacob@joinatlas.xyz>
2024-05-12 09:49:27 -0400

    update messages

--- src/data/reminders.ts
+++ src/data/reminders.ts
@@ -1,7 +1,7 @@
 export default [
 
     ['0 9 * * *', "Every day begins with so much potential to explore and engage on Farcaster. Atlas is here to help you do both."],
-    ['0 13 * * *', "Whether you're here to read, or find the best conversations to jump into, Atlas is here to help."],
+    ['0 12 * * *', "Whether you're here to read, or find the best conversations to jump into, Atlas is here to help."],
     ['0 15 * * *', "On Farcaster, ideas quickly become movements. Atlas is here to keep you up to speed."],
     ['0 18 * * *', "There's a lot happening on Farcaster. Atlas is here to help you find what matters most to you."],
     ['0 21 * * *', "As the day winds down, Atlas is here to guide your Farcaster reflections."]
