e83845fe57cd0465278039e7f022670ab68b52a8
Jacob <jacob@joinatlas.xyz>
2024-05-24 15:24:05 -0400

    conform messages to prior

--- src/data/reminders.ts
+++ src/data/reminders.ts
@@ -1,5 +1,9 @@
 export default [
 
-    ['0 9 * * *', "Atlas is your AI companion for Warpcast. Message @jrf to learn more."],
+    ['0 9 * * *', "Every day begins with so much potential to explore and engage on Farcaster. Atlas is here to help you do both."],
+    ['0 12 * * *', "Whether you're here to read, or find the best conversations to jump into, Atlas is here to help."],
+    ['0 15 * * *', "On Farcaster, ideas quickly become movements. Atlas is here to keep you up to speed."],
+    ['0 18 * * *', "There's a lot happening on Farcaster. Atlas is here to help you find what matters most to you."],
+    ['0 21 * * *', "As the day winds down, Atlas is here to guide your Farcaster reflections."]
 
 ] as [string, string][];
